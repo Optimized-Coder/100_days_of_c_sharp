129c6b93c556a2694d2fab2723fe3341330a8aac
Josh Shepherd <joshuashepherd877@gmail.com>
2023-03-30 14:26:17 +0100

    day 2 - number methods

---
+++ day_2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Day 2
+
+using System;
+
+namespace Day2
+{
+        class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Increment & decrement
+            int steps = 0;
+            steps += 2;
+            Console.WriteLine(steps);
+            steps -= 1;
+            Console.WriteLine(steps);
+
+            // number methods
+            Console.WriteLine("===== Number Methods =====");
+            int numberOne = 52;
+            double numberTwo = -31;
+            double numberThree = 46.9;
+            Console.WriteLine($"Number {numberOne}");
+            Console.WriteLine($"Square Root: {Math.Sqrt(numberOne)}");
+            Console.WriteLine($"Absolute: {Math.Abs(numberTwo)}");
+            Console.WriteLine($"Lowest Number: {Math.Min(numberOne, numberThree)}");
+            Console.WriteLine($"Highest Number: {Math.Max(numberOne, numberThree)}");
+            Console.WriteLine($"Rounded down: {Math.Floor(numberThree)}");
+            Console.WriteLine($"Rounded up: {Math.Ceiling(numberThree)}");
+        }
+    } 
+}
