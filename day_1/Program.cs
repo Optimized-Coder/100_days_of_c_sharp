eec48f14ab7b8fe806932dfdda25ff535cb00fbc
Josh Shepherd <joshuashepherd877@gmail.com>
2023-03-30 13:43:28 +0100

    day one from yesterday

---
+++ day_1/Program.cs
@@ -0,0 +1,9 @@
+﻿string name = "Josh";
+int age = 26;
+string language = "C#";
+
+string message = @$"My name is {name} and I am {age} years old
+
+        Welcome to my #100 days of code! I will be learning {language} and the .NET stack";
+
+Console.WriteLine(message);
